842b6f0f206604fdc2083e12c70572a0b3f2ce33
MuriloSG <muriloprojetosti@gmail.com>
2024-06-28 17:09:34 -0300

    refactor: refactor auth guard

--- src/auth/guards/auth.guard.ts
+++ src/auth/guards/auth.guard.ts
@@ -1,20 +1,22 @@
 import { CanActivate, ExecutionContext, Injectable } from "@nestjs/common";
-import { Observable } from "rxjs";
 import { AuthService } from "../auth.service";
+import { UsersService } from "src/users/users.service";
 
 @Injectable()
 export class AuthGuard implements CanActivate {
-  constructor(private readonly authService: AuthService) {}
+  constructor(
+    private readonly authService: AuthService,
+    private readonly usersService: UsersService,
+  ) {}
 
-  canActivate(
-    context: ExecutionContext,
-  ): boolean | Promise<boolean> | Observable<boolean> {
+  async canActivate(context: ExecutionContext) {
     const request = context.switchToHttp().getRequest();
     const { authorization } = request.headers;
     try {
       const data = this.authService.checkToken(
         (authorization ?? "").split(" ")[1],
       );
+      request.user = await this.usersService.show(data.sub);
       request.tokenPayload = data;
       return true;
     } catch (error) {
