969dac096d67a051eb45a6dd66308d8bfbf8301f
MuriloSG <muriloprojetosti@gmail.com>
2024-06-28 10:32:15 -0300

    feat: implement authGuard

--- src/auth/guards/auth.guard.ts
+++ src/auth/guards/auth.guard.ts
@@ -1,15 +1,21 @@
 import { CanActivate, ExecutionContext, Injectable } from "@nestjs/common";
 import { Observable } from "rxjs";
+import { AuthService } from "../auth.service";
 
 @Injectable()
 export class AuthGuard implements CanActivate {
+  constructor(private readonly authService: AuthService) {}
+
   canActivate(
     context: ExecutionContext,
   ): boolean | Promise<boolean> | Observable<boolean> {
     const request = context.switchToHttp().getRequest();
     const { authorization } = request.headers;
-    console.log(authorization);
     try {
+      const data = this.authService.checkToken(
+        (authorization ?? "").split(" ")[1],
+      );
+      request.tokenPayload = data;
       return true;
     } catch (error) {
       return false;
